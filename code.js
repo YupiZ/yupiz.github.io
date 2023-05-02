7037c3a332ac8852b0c5527c741bce39030e4415
YupiZ <101301115+YupiZ@users.noreply.github.com>
2023-05-02 06:51:56 -0400

    hide class added

--- code.js
+++ code.js
@@ -1,12 +1,18 @@
+window.onload = detectDevice;
+
 function detectDevice() {
     const userAgent = navigator.userAgent;
     const iOS = /iPad|iPhone|iPod/.test(userAgent);
     const android = /Android/i.test(userAgent);
 
     if (iOS) {
-    document.getElementById('androidBtn').style.display = 'none';
+        document.getElementById('androidBtn').classList.add('hide');
+        document.getElementById('appleBtn').classList.remove('hide');
     } else if (android) {
-    document.getElementById('appleBtn').style.display = 'none';
+        document.getElementById('appleBtn').classList.add('hide');
+        document.getElementById('androidBtn').classList.remove('hide');
+    } else {
+        document.getElementById('appleBtn').classList.remove('hide');
+        document.getElementById('androidBtn').classList.remove('hide');
     }
 }
-window.onload = detectDevice;
